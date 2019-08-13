618cf84572fc4193f9c098ae9e61ea03c68b3664
Vadim Veksler <reyzele91@gmail.com>
2019-08-13 23:40:04 +0300

    .

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -9,8 +9,72 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 
 export default (state = [], action) => {
   switch (action.type) {
+    case CREATE_NEW_ORDER:
+      return [
+        ...state,
+        { ...action.payload, ingredients: [], position: 'clients' }
+      ];
+    case MOVE_ORDER_NEXT:
+      return state.map(order =>
+        order.id === action.payload ? moveNext(order) : order
+      );
+    case MOVE_ORDER_BACK:
+      return state.map(order =>
+        order.id === action.payload ? moveBack(order) : order
+      );
+    case ADD_INGREDIENT:
+      return state;
     default:
       return state;
+  }
+};
+
+const moveNext = order => {
+  switch (order.position) {
+    case 'clients':
+      return {
+        ...order,
+        position: 'conveyor_1'
+      };
+    case 'conveyor_1':
+      return {
+        ...order,
+        position: 'conveyor_2'
+      };
+    case 'conveyor_2':
+      return {
+        ...order,
+        position: 'conveyor_3'
+      };
+    case 'conveyor_3':
+      return {
+        ...order,
+        position: 'conveyor_4'
+      };
+    default:
+      return order;
+  }
+};
+
+const moveBack = order => {
+  switch (order.position) {
+    case 'conveyor_2':
+      return {
+        ...order,
+        position: 'conveyor_1'
+      };
+    case 'conveyor_3':
+      return {
+        ...order,
+        position: 'conveyor_2'
+      };
+    case 'conveyor_4':
+      return {
+        ...order,
+        position: 'conveyor_3'
+      };
+    default:
+      return order;
   }
 };
 
